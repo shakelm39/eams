d8b18ffaa3303ca4cd49622303ac9d184089844f
Md Shakel Kawsar <shakelm39@gmail.com>
2024-08-31 22:25:48 +0600

    Dynamically employee insert

--- add_employee.php
+++ add_employee.php
@@ -1,4 +1,29 @@
-<?php include ('inc/header.php') ?>
+<?php 
+    include ('inc/header.php');
+    include('lib/Employee.php');
+ ?>
+<?php 
+
+    $emp = new Employee();
+ 
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $name = $_POST['emp_name'];
+        $emp_id = $_POST['emp_id'];
+
+      
+        $insert_data = $emp->insertEmployee($name, $emp_id);
+
+       
+        
+    }
+
+?>
+<?php 
+    if(isset($insert_data)){
+       
+        echo $insert_data;
+    }
+?>
  <div class="card">
     <div class="card-header">
         <a href="add_employee.php" class="btn btn-success">Add Employee</a>
@@ -8,11 +33,11 @@
         <form action="" method="POST">
             <div class="form-group">
                 <label for="emp_name">Employee Name</label>
-                <input class="form-control" type="text" name="emp_name" id="emp_name" required>
+                <input class="form-control" type="text" name="emp_name" id="emp_name">
             </div>
             <div class="form-group">
                 <label for="emp_id">Employee Id</label>
-                <input class="form-control" type="text" name="emp_id" id="emp_id" required>
+                <input class="form-control" type="text" name="emp_id" id="emp_id">
             </div>
             <div class="form-group">
                 <input class="btn btn-primary mt-2" class="form-control" type="submit" name="submit" value="Submit">
--- lib/Employee.php
+++ lib/Employee.php
@@ -20,6 +20,31 @@ class Employee{
         $result = $this->db->select($query);
         return $result;
     }
+    public function insertEmployee($name, $emp_id){
+        $name = mysqli_real_escape_string($this->db->link, $name);
+        $emp_id = mysqli_real_escape_string($this->db->link, $emp_id);
+
+        if(empty($name) || empty($emp_id)){
+            $msg = "<div class='alert alert-danger'><strong>Error!</strong> Field must not be empty!!</div>";
+            return $msg;
+        }else{
+            // employee table insert data query
+            $emp_query = "INSERT INTO employee(name,emp_id) VALUES('$name','$emp_id')";
+            $insert_emp = $this->db->insert($emp_query);
+
+            // tbl_attendance table insert only emp id  query
+            $att_query = "INSERT INTO tbl_attendance(emp_id) VALUES('$emp_id')";
+            $insert_emp = $this->db->insert($att_query);
+
+            if($insert_emp){
+                $msg = "<div class='alert alert-success'><strong>Success!</strong> Successfully employee data inserted!!</div>";
+                return $msg;
+            }else{
+                $msg = "<div class='alert alert-danger'><strong>Error!</strong> Data not inserted!!</div>";
+                return $msg;
+            }
+        }
+    }
 }
 
 ?>
